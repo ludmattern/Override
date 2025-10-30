0d019645021b85c6806e12dad5b49e2ca73aee1d
jgavairo <jgavairo@DebianForRainFall.myguest.virtualbox.org>
2025-10-30 11:13:51 +0100

    level08

---
+++ level08/source.c
@@ -0,0 +1,66 @@
+undefined8 main(int param_1,undefined8 *param_2)
+
+{
+  char cVar1;
+  int __fd;
+  int iVar2;
+  FILE *pFVar3;
+  FILE *__stream;
+  ulong uVar4;
+  char *pcVar5;
+  long in_FS_OFFSET;
+  byte bVar6;
+  char local_79;
+  char local_78 [104];
+  long local_10;
+  
+  bVar6 = 0;
+  local_10 = *(long *)(in_FS_OFFSET + 0x28);
+  local_79 = -1;
+  if (param_1 != 2) {
+    printf("Usage: %s filename\n",*param_2);
+  }
+  pFVar3 = fopen("./backups/.log","w");
+  if (pFVar3 == (FILE *)0x0) {
+    printf("ERROR: Failed to open %s\n","./backups/.log");
+                    /* WARNING: Subroutine does not return */
+    exit(1);
+  }
+  log_wrapper(pFVar3,"Starting back up: ",param_2[1]);
+  __stream = fopen((char *)param_2[1],"r");
+  if (__stream == (FILE *)0x0) {
+    printf("ERROR: Failed to open %s\n",param_2[1]);
+                    /* WARNING: Subroutine does not return */
+    exit(1);
+  }
+  builtin_strncpy(local_78,"./backups/",0xb);
+  uVar4 = 0xffffffffffffffff;
+  pcVar5 = local_78;
+  do {
+    if (uVar4 == 0) break;
+    uVar4 = uVar4 - 1;
+    cVar1 = *pcVar5;
+    pcVar5 = pcVar5 + (ulong)bVar6 * -2 + 1;
+  } while (cVar1 != '\0');
+  strncat(local_78,(char *)param_2[1],99 - (~uVar4 - 1));
+  __fd = open(local_78,0xc1,0x1b0);
+  if (__fd < 0) {
+    printf("ERROR: Failed to open %s%s\n","./backups/",param_2[1]);
+                    /* WARNING: Subroutine does not return */
+    exit(1);
+  }
+  while( true ) {
+    iVar2 = fgetc(__stream);
+    local_79 = (char)iVar2;
+    if (local_79 == -1) break;
+    write(__fd,&local_79,1);
+  }
+  log_wrapper(pFVar3,"Finished back up ",param_2[1]);
+  fclose(__stream);
+  close(__fd);
+  if (local_10 != *(long *)(in_FS_OFFSET + 0x28)) {
+                    /* WARNING: Subroutine does not return */
+    __stack_chk_fail();
+  }
+  return 0;
+}
